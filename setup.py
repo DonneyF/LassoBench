ba0f3602fb2ef0b63b00bcfd5451e28b28850562
Donney Fan <donney.fan@hotmail.com>
2025-12-16 17:27:45 -0800

    Make ax and plotting libraries optional.

--- setup.py
+++ setup.py
@@ -8,11 +8,12 @@ setup(name='LassoBench',
           'celer @ git+https://github.com/mathurinm/celer.git@eef3bb5',
           'pyDOE',
           'libsvmdata',
-          'ax-platform',
-          'matplotlib>=2.0.0',
           'numpy>=1.12',
           'scipy>=0.18.0',
           'scikit-learn>=0.21',
-          'seaborn>=0.7',
           'pyDOE>=0.3.8'],
+      extras_require={
+          'alebo_example': ["ax-platform"],
+          'plot': ['seaborn>=0.7', 'matplotlib>=2.0.0']
+      }
       )
